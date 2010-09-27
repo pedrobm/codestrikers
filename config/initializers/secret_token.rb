531514b35e203c76e80f37fb23ecbb499476a7d6
pedro <pedro@pedro-laptop>
2010-09-27 20:29:20 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodestrikersFrontoffice::Application.config.secret_token = 'a45d86692aef2931880d6cd58ed47a90b4a255d2ae932bfca7a8801c065f1bab335ff6e2e5a5c302bfdb8c505a15a063b37bf814035840dca9e1bf5da37b02f1'
